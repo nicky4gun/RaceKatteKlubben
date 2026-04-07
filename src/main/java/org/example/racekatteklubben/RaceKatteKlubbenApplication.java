719f24ae8288219d812e45d4fbf6c31084c29542
nicky4gun <nicklas.stagesorensen@outlook.dk>
2026-04-07 09:37:05 +0200

    -add to github

---
+++ src/main/java/org/example/racekatteklubben/RaceKatteKlubbenApplication.java
@@ -0,0 +1,13 @@
+package org.example.racekatteklubben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceKatteKlubbenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceKatteKlubbenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/racekatteklubben/RaceKatteKlubbenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.racekatteklubben;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceKatteKlubbenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
